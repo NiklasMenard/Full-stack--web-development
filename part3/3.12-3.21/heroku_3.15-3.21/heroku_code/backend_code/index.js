0326b4710ba5f8f968ac1f72a467668e66336a75
Niklas Menard <nmenard@localhost.localdomain>
2020-07-31 23:52:45 +0300

    Made the directory structure clearer and according to the assingments

---
+++ part3/3.12-3.21/heroku_3.15-3.21/heroku_code/backend_code/index.js
@@ -0,0 +1,104 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const Person = require('./models/person')
+const { response, json } = require('express')
+
+app.use(express.json())
+app.use(express.static('build'))
+
+app.get('/info', (req, res, next) => {
+
+  Person
+    .estimatedDocumentCount()
+    .then(count => {
+      const length = count
+      const date = new Date()
+      res.send('Phonebook has info for ' + length + ' people ' + date)
+    })
+    .catch(error => next(error))
+})
+
+
+app.get('/api/persons', (req, res) => {
+  Person.find({}).then(person => {
+    res.json(person)
+  })
+})
+
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/persons', (req, res, next) => {
+  console.log(req.body)
+  const body = req.body
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save()
+  .then(savedPerson => {
+    res.json(savedPerson)
+  })
+  .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'Validation error') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+app.use(errorHandler)
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
