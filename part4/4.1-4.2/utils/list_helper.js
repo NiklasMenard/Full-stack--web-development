bad5fef25d363f6b57bcebb472489e41fb9b0dc5
Your Name <nmenard@localhost.localdomain>
2020-10-11 14:09:13 +0300

    continuing part 4

---
+++ part4/4.1-4.2/utils/list_helper.js
@@ -0,0 +1,48 @@
+
+const dummy = (blogs) => {
+    return 1;
+}
+
+const totalLikes = (blogs) => {
+    const total = blogs.map(blogs => blogs.likes).reduce((prev, next) => prev + next)
+    return total
+}
+
+const favoriteBlog = (blogs) => {
+    const mostLikes = Math.max(...blogs.map(({ likes }) => likes))
+    const favoriteBlog = blogs.find(({ likes }) => likes === mostLikes)
+    return favoriteBlog
+}
+
+const mostBlogsAuthor = (blogs) => {
+
+    const mostBlogsAuthor = [...blogs.sort((a,b) =>
+          blogs.filter(v => v===a).length
+        - blogs.filter(v => v===b).length
+    )].pop()
+    const amountOfblogs = blogs.filter(blog => blog.author === mostBlogsAuthor.author)
+    return {author: mostBlogsAuthor.author, amount: amountOfblogs.length}
+}
+
+
+const mostLikesAuthor = (blogs) => {
+    let result = blogs.reduce((acc, obj)=>{
+        let existingBlog = acc.find(blog => blog.author === obj.author);
+        if(existingBlog){
+            existingBlog.likes += obj.likes;
+          return acc;
+        } 
+        acc.push(obj);
+        return acc;
+      }, []);
+      return favoriteBlog(result)
+}
+
+     
+module.exports = {
+    dummy,
+    totalLikes,
+    favoriteBlog,
+    mostBlogsAuthor,
+    mostLikesAuthor
+}
